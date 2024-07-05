c04873a90cbf20567e400a2c17e16f5cb70e6ad3
ktcotz <naskret.kamil@gmail.com>
2024-07-05 22:21:28 +0200

    05.07.2024
    Inicjalizacja world wise.

---
+++ Lipiec/JonasReact/src/advanced/spa/components/message/Message.tsx
@@ -0,0 +1,13 @@
+import styles from "./Message.module.css";
+
+type MessageProps = { message: string };
+
+function Message({ message }: MessageProps) {
+  return (
+    <p className={styles.message}>
+      <span role="img">👋</span> {message}
+    </p>
+  );
+}
+
+export default Message;
